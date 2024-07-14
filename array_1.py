8ca5601ac11d096653ca8b2933072456c9e01f05
Aniket Prakash <aniketprakashmgs@gmail.com>
2024-07-14 21:18:07 +0530

    Initial commit

---
+++ array_1.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr1 = np.array([1,2,3,4,5])  #1D array
+arr2 = np.array([[1,2,3],[4,5,6]])  #2D array
+arr3 = np.array([[0,1,2],[4,5,6],[7,8,9]])
+print(arr1)
+print(arr2)
+print(arr3)
